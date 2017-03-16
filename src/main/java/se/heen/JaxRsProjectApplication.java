4bef5bdd38d4689f6e59a71a7e6525223714d024
Henrik Engstrand <Henrik@Henriks-MacBook-Pro.local>
2017-03-16 09:12:35 +0100

    Random files

---
+++ src/main/java/se/heen/JaxRsProjectApplication.java
@@ -0,0 +1,12 @@
+package se.heen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxRsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaxRsProjectApplication.class, args);
+	}
+}
