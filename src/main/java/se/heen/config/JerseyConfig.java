e443b3d686fad232784643aa835775582fdf6632
Heenkee4 <henrikengstrand@gmail.com>
2017-03-20 11:55:05 +0100

    Updated JerseyConfig

--- src/main/java/se/heen/config/JerseyConfig.java
+++ src/main/java/se/heen/config/JerseyConfig.java
@@ -4,11 +4,13 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
 import resource.IssueResource;
+import se.heen.repository.impl.InMemoryIssueRepository;
 
 @Component
 public final class JerseyConfig extends ResourceConfig {
 
 	public JerseyConfig() {
 		register(IssueResource.class);
+		register(InMemoryIssueRepository.class);
 		}
 	}
